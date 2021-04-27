68ede88740fcfd1928b2d33f2deb3920146c78be
Roman Sekot <roman.sekot@avocodo.com>
2021-04-27 20:32:41 +0200

    Spring Initializr for Backend Spring Boot Application

---
+++ src/main/java/com/github/sekkycodes/softwarequest/SoftwareQuestApplication.java
@@ -0,0 +1,13 @@
+package com.github.sekkycodes.softwarequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sekkycodes/softwarequest/SoftwareQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sekkycodes.softwarequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
